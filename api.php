525410f58e9459eb4cb5df7891fdf4bbc3b1c5eb
JoseCabrejoVillarCampus <cabre920903@hotmail.com>
2023-06-01 21:52:12 -0500

    feat: :fire: Cambio decodigo de la calculadora
    cambio en la mayor parte del codigo, por uno mas optimo,  eliminando funciones y asignando solo un metodo

--- api.php
+++ api.php
@@ -1,38 +1,61 @@
 <?php
-    header('Content-type:application/json');
 
-    $nums=$_POST;
-    class Calculadora{
-        public function suma(){
-            global $nums;
-            if (in_array('+',$nums)) {
-                if (($clave = array_search('+', $nums)) !== false) {
-                    unset($nums[$clave]);}
-                $result= array_sum($nums);
+class Calculadora
+{
+    public function calcular($operacion)
+    {
+        $operadores = ['+', '-', '*', '/'];
+        $resultado = null;//aca almacenamos el resultado
+
+        foreach ($operadores as $operador) {//iteramos cada operador
+            if (strpos($operacion, $operador) !== false) {//verifica si un operador específico está presente 
+                $partes = explode($operador, $operacion);//aca usamos explode para separar antes y desps del operador 
+
+                if (count($partes) !== 2) {
+                    echo $operacion;
+                    return;//validamos que al menos sean dos numeros para poder realizar la operacion
+                }
+
+                $num1 = floatval($partes[0]);//con floatval obtenemos el valor float de los valores en las posiciones deseada del array
+                $num2 = floatval($partes[1]);
+
+                switch ($operador) {
+                    case '+':
+                        $resultado = ($resultado === null) ? $num1 + $num2 : $resultado + $num2;
+                        break;
+                    case '-':
+                        $resultado = ($resultado === null) ? $num1 - $num2 : $resultado - $num2;
+                        break;
+                    case '*':
+                        $resultado = ($resultado === null) ? $num1 * $num2 : $resultado * $num2;
+                        break;
+                    case '/':
+                        if ($num2 != 0) {
+                            $resultado = ($resultado === null) ? $num1 / $num2 : $resultado / $num2;
+                        } else {
+                            echo "La división por cero no es valida";
+                            return;
+                        }
+                        break;
+                }
             }
         }
-        public function resta(){
-            global $nums;
-            if (in_array('-',$nums)) {
-                $result= 0;
-            }
-        }
-        public function por(){
-            global $nums;
-            if (in_array('*',$nums)) {
-                $result= 0;
-            }
-        }
-        public function div(){
-            global $nums;
-            if (in_array('+',$nums)) {
-                $result= 0;
-            }
+
+        if ($resultado !== null) {
+            echo $resultado;
+        } else {
+            echo $operacion;
         }
     }
-    
+}
 
-
-    
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    if (isset($_POST['num'])) {
+        $operacion = $_POST['num'][0];
+        
+        $calculadora = new Calculadora();
+        $calculadora->calcular($operacion);
+    }
+}
 
 ?>
